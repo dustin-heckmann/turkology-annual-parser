665a187a5a7a5ea9e11ef84180fa85c82d2ad272
Dustin Heckmann <kontakt@dustin-heckmann.de>
2019-08-21 21:13:25 +0200

    Use strings for volumes
    Previously, both strings and integers had been used because of the double volume 22-23. Now, all volumes are extracted as strings

--- turkology-annual-parser/paragraph/paragraph_extraction.py
+++ turkology-annual-parser/paragraph/paragraph_extraction.py
@@ -6,7 +6,7 @@ from paragraph.wmlparser import WMLParser
 def extract_paragraphs(volume_filename):
     volume_number = volume_filename.split("/")[-1].split("_")[0][2:]
     if volume_number.isdigit():
-        volume_number = int(volume_number)
+        volume_number = str(int(volume_number))
 
     parser = WMLParser(volume_filename)
     paragraph_index = 0
