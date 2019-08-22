f55045332f8410270b8fe7104657c6220740fc70
Dustin Heckmann <kontakt@dustin-heckmann.de>
2019-08-22 21:18:50 +0200

    Remove unused small caps ranges

--- turkology-annual-parser/citation/assembly.py
+++ turkology-annual-parser/citation/assembly.py
@@ -22,8 +22,6 @@ def assemble_citations(paragraphs):
                 _timestamp=datetime.now(),
                 _creator='<initial>',
             )
-            if 'small_caps_ranges' in paragraph:
-                current_citation['small_caps_ranges'] = paragraph['small_caps_ranges']
         elif paragraph['type'] and paragraph['type'] == 'amendment':
             current_citation.setdefault('amendments', []).append(paragraph['text'])
     if current_citation:
