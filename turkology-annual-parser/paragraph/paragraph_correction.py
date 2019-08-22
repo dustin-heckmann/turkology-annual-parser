e177438f64524d353837d863e9c02fc60d523452
Dustin Heckmann <kontakt@dustin-heckmann.de>
2019-08-22 18:04:35 +0200

    Add corrections for volume 3

--- turkology-annual-parser/paragraph/paragraph_correction.py
+++ turkology-annual-parser/paragraph/paragraph_correction.py
@@ -7,7 +7,8 @@ def correct_paragraphs(paragraphs):
     if volume == '2':
         paragraphs[3088] = merge_paragraphs(paragraphs[3088:3090])
         del paragraphs[3089]
-    #print(paragraphs)
+    elif volume == '3':
+        paragraphs = paragraphs[:700] + [merge_paragraphs(paragraphs[701:707])] + paragraphs[707:]
     return paragraphs
 
 
