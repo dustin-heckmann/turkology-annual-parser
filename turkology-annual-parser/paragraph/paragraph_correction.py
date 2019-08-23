d10d13304e7dcb6c6a0660139cb3c8c6442c3258
Dustin Heckmann <kontakt@dustin-heckmann.de>
2019-08-23 17:01:45 +0200

    Add correction (merging paragraphs) for volume 6

--- turkology-annual-parser/paragraph/paragraph_correction.py
+++ turkology-annual-parser/paragraph/paragraph_correction.py
@@ -9,6 +9,8 @@ def correct_paragraphs(paragraphs):
         del paragraphs[3089]
     elif volume == '3':
         paragraphs = paragraphs[:700] + [merge_paragraphs(paragraphs[701:707])] + paragraphs[707:]
+    elif volume == '6':
+        paragraphs = paragraphs[:1964] + [merge_paragraphs([paragraphs[1964], paragraphs[1968]])] + paragraphs[1965:1968] + paragraphs[1969:]
     elif volume == '10':
         paragraphs = paragraphs[:461] + [merge_paragraphs([paragraphs[461], paragraphs[465]])] + paragraphs[462:465] + paragraphs[466:]
     elif volume == '11':
