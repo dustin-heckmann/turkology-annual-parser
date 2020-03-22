d131653c03b990111a84f105b58ca8760da7cc8e
Dustin Heckmann <kontakt@dustin-heckmann.de>
2020-03-22 12:57:33 +0100

    Also create jsonlines export
    http://jsonlines.org/

--- turkology-annual-parser/repositories/JsonRepository.py
+++ turkology-annual-parser/repositories/JsonRepository.py
@@ -1,6 +1,7 @@
-import datetime
 import json
-from typing import Iterator
+from typing import Iterator, List
+
+import jsonlines
 
 from citation.citation import Citation
 from repositories.BaseRepository import BaseRepository
@@ -11,9 +12,22 @@ class JsonRepository(BaseRepository):
         self._filename = filename
 
     def write_citations(self, citations: Iterator[Citation]):
+        citations = [self._citation_as_dict(citation) for citation in citations]
+        self._write_json(citations)
+        self._write_json_lines(citations)
+
+        with open(self._filename + 'l', 'w') as storage_file:
+            writer = jsonlines.Writer(storage_file)
+            for citation in citations:
+                writer.write(citation)
+
+    def _write_json(self, citations: List[Citation]):
         with open(self._filename, 'w') as storage_file:
             json.dump(
-                list(map(self._citation_as_dict, citations)),
-                storage_file,
+                citations,
+                storage_file
             )
 
+    def _write_json_lines(self, citations: List[Citation]):
+        with jsonlines.open(self._filename + 'l', 'w') as writer:
+            writer.write_all(citations)
